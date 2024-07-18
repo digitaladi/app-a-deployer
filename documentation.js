5d1ba968d062ada598db01d089a564e5edd79a90
Aladi TIMERA <aladi.timera@eren.lan>
2024-07-18 12:34:57 +0200

    premiere commit de l'application app-a-deployer

---
+++ documentation.js
@@ -0,0 +1,27 @@
+/* 
+
+((((DÉPLOYER SON APPLICATION REACT NATIVE))))))
+
+Utilitaires utilisés :  github et netifly
+
+
+créer un dépot  sur github du projet app-a-deployer
+
+1 Aller sur github et creer le nom du repot et suivre ces etapes : 
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/digitaladi/app-a-deployer.git
+git push -u origin main
+
+
+et maintenant le code de "app-a-deployer" est dans github
+
+
+
+
+
+
+*/
+
---
+++ screens/Rules.js
@@ -0,0 +1,24 @@
+import { Text, View } from "react-native";
+
+
+
+export default function Rules() {
+
+return(
+
+    <View style={{margin:20}}>
+
+<Text style={{fontSize:50, fontWeight:'bold', padding:20}}>   Fake-nutrinet   </Text>
+
+    <Text style={{fontSize:20, fontWeight:'bold', padding:10, color:"red"}}>   Régles de confidentialité :   </Text>
+    <Text>  
+        
+        Cette application utilise vos données de localisation pour mle fonctionnenement de l'application.
+        Ces données ne sont ni stockées ni partagées par nous mêmes à des personnes tiers. 
+        Les données sont partagées par vous mêmes aux personnes de votre choix lorsque vous utilisez l'application
+        
+         </Text>
+    </View>
+
+)
+}
